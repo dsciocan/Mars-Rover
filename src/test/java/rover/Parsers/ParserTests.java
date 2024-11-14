d0f95b0b2bc2679cf9bfc65afa4daee3c7a8262b
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2024-11-14 16:54:54 +0000

    test: add PositionParser tests

--- src/test/java/rover/Parsers/ParserTests.java
+++ src/test/java/rover/Parsers/ParserTests.java
@@ -2,8 +2,10 @@ package rover.Parsers;
 
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import rover.Cardinal;
 import rover.Instruction;
 import rover.Plateau;
+import rover.Position;
 
 import java.util.ArrayList;
 
@@ -65,6 +67,25 @@ public class ParserTests {
         assertEquals(expected, CoordinateParser.generateCoordinates(testString));
     }
 
+    //Position Parser Tests
 
+    @Test (expected = IllegalArgumentException.class)
+    public void testGenerateStartingPosition_fullyInvalidString() {
+        String testString = "abcd hshks a45";
+        PositionParser.generateStartingPosition(testString);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testGenerateStartingPosition_PartiallyInvalidString() {
+        String testString = "3 4 N abcd hshks";
+        PositionParser.generateStartingPosition(testString);
+    }
+
+    @Test
+    public void testGenerateStartingPosition_ValidString() {
+        String testString = "3 4 n";
+        Position expected = new Position(3,4, Cardinal.N);
+        assertEquals(expected, PositionParser.generateStartingPosition(testString));
+    }
 
 }
