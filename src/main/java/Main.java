48fe98d40741d26822601e6f6e16458c2ce93c12
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2024-11-15 13:52:35 +0000

    feat: add Main class usage example

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,20 @@
 import rover.Exploration;
 import rover.Parsers.CoordinateParser;
-import rover.Parsers.MovesetParser;
 import rover.Parsers.PositionParser;
 import rover.Plateau;
 import rover.Rover;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Main {
 
     public static void main(String[] args) {
-
+        String[] testArray = {"5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"};
+        Plateau plateau = CoordinateParser.generatePlateau(testArray[0]);
+        Rover roverOne = new Rover(PositionParser.generateStartingPosition(testArray[1]));
+        Exploration explorationOne = new Exploration(roverOne, plateau, testArray[2]);
+        explorationOne.executeMoveset();
+        Rover roverTwo = new Rover(PositionParser.generateStartingPosition(testArray[3]));
+        Exploration explorationTwo = new Exploration(roverTwo, plateau, testArray[4]);
+        explorationTwo.executeMoveset();
     }
 
 }
