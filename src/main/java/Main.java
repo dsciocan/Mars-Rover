3baf1a6e9c2b1f4b34d627506fc803e546663895
Stef Daniela Ciocan <daniela.ciocan137@gmail.com>
2024-11-15 15:39:38 +0000

    refactor: update main to contain UI running option

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,21 @@ import rover.Parsers.CoordinateParser;
 import rover.Parsers.PositionParser;
 import rover.Plateau;
 import rover.Rover;
+import rover.UI.RoverUI;
+import rover.UI.Setup;
 
 public class Main {
 
     public static void main(String[] args) {
-        String[] testArray = {"5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"};
-        Plateau plateau = CoordinateParser.generatePlateau(testArray[0]);
-        Rover roverOne = new Rover(PositionParser.generateStartingPosition(testArray[1]));
-        Exploration explorationOne = new Exploration(roverOne, plateau, testArray[2]);
-        explorationOne.executeMoveset();
-        Rover roverTwo = new Rover(PositionParser.generateStartingPosition(testArray[3]));
-        Exploration explorationTwo = new Exploration(roverTwo, plateau, testArray[4]);
-        explorationTwo.executeMoveset();
+//        String[] testArray = {"5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"};
+//        Plateau plateau = CoordinateParser.generatePlateau(testArray[0]);
+//        Rover roverOne = new Rover(PositionParser.generateStartingPosition(testArray[1]));
+//        Exploration explorationOne = new Exploration(roverOne, plateau, MovesetParser.generateMoveset(testArray[2]));
+//        explorationOne.executeMoveset();
+//        Rover roverTwo = new Rover(PositionParser.generateStartingPosition(testArray[3]));
+//        Exploration explorationTwo = new Exploration(roverTwo, plateau, MovesetParser.generateMoveset(testArray[4]));
+//        explorationTwo.executeMoveset();
+        RoverUI.runRover();
     }
 
 }
